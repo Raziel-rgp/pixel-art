3efecb8b78f254f9d4ad22447031956aee37cc59
fawl_03 <razidias@gmail.com>
2022-05-09 17:18:10 -0300

    att 1.7.2 cores aleatorias

--- script.js
+++ script.js
@@ -1,18 +1,57 @@
-window.onload = createPixels()
+const paletaList = document.querySelector('#color-palette')
+    for(let index = 0; index < 4; index++){
+        let paletaCrianca = document.createElement('button');
+        paletaCrianca.className = 'color';
+        paletaList.appendChild(paletaCrianca);
 
+    }
+window.onload = createPixels();
+const blocoCor = document.getElementsByClassName('color')
 function createPixels() {
-    createSelection()
-    let pixel = document.querySelector('#pixel-board');
+    const pixel = document.querySelector('#pixel-board');
+    
+
     for (let index = 0; index < 25; index += 1 ) {
-        let pixelCrianca = document.createElement('button');
+        
+        pixelCrianca = document.createElement('button');
         pixelCrianca.className = 'pixel';
         pixel.appendChild(pixelCrianca);
     }
+    crearCor();
     
-
 }
-function createSelection() {
-    let colorSelected = document.createElement('div');
-    colorSelected.className = 'selected';
-    colorSelected.style.backgroundColor = 'black';
+function crearCor(){
+    let blocoCor = document.getElementsByClassName('color');
+    for(let index = 0; index < blocoCor.length; index += 1){
+        const corAle = [];
+        const corgb = 3;
+        for (let rgbIndex = 0; rgbIndex < corgb; rgbIndex += 1 ){
+            corAle.push(Math.floor(Math.random() * 254))
+        }
+        blocoCor[index].style.backgroundColor = `rgb(${corAle})`;
+    }
+    blocoCor[0].style.backgroundColor = 'rgb(0 , 0 , 0)';
+    blocoCor[0].classList.add('selected')
+}
+
+let listaDaPaleta = document.querySelector('.color');
+for(let index = 0; index < listaDaPaleta.length; index++){
+    listaDaPaleta[index].addEventListener('click', select())
+}
+function select (){
+    const listaDaPaleta = document.querySelector('.color');
+    let selecao = document.querySelector('selected')
+    console.log('Funfou')
+    for(let index = 0; index < listaDaPaleta.length; index++){
+        console.log('Funfou')
+        listaDaPaleta[index].classList.add('selected')
+        selecao = listaDaPaleta[index].style.backgroundColor;  
+        for(let i = 0; i < listaDaPaleta.length; i++) {
+            console.log('Funfou')
+            if(listaDaPaleta[index] !== listaDaPaleta[i] && listaDaPaleta[i].classList.contains('selected')){
+                listaDaPaleta[i].classList.remove('select');
+            }
+        }
+    }
+    console.log('Funfou')
 }
