04d325c539203eb2db6a129a0159be651af52177
fawl_03 <razidias@gmail.com>
2022-05-17 21:02:14 -0300

    att 1.10.0

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 const paletaList = document.querySelector('#color-palette')
-    for(let index = 0; index < 4; index++){
-        let paletaCrianca = document.createElement('button');
-        paletaCrianca.className = 'color';
-        paletaList.appendChild(paletaCrianca);
+for(let index = 0; index < 4; index++){
+    let paletaCrianca = document.createElement('li');
+    paletaCrianca.className = 'color';
 
-    }
-window.onload = createPixels();
+    paletaList.appendChild(paletaCrianca);
+}
+
+window.onload = iniciar()
+function iniciar() {
+    const selectedPaint = 'rgb(0, 0, 0)'
+    createPixels();
+}
 const blocoCor = document.getElementsByClassName('color')
 function createPixels() {
     const pixel = document.querySelector('#pixel-board');
-    
-
     for (let index = 0; index < 25; index += 1 ) {
-        
-        pixelCrianca = document.createElement('button');
+        pixelCrianca = document.createElement('li');
         pixelCrianca.className = 'pixel';
         pixel.appendChild(pixelCrianca);
     }
     crearCor();
-    
 }
-function crearCor(){
+function crearCor() {
     let blocoCor = document.getElementsByClassName('color');
     for(let index = 0; index < blocoCor.length; index += 1){
         const corAle = [];
@@ -33,25 +34,31 @@ function crearCor(){
     blocoCor[0].style.backgroundColor = 'rgb(0 , 0 , 0)';
     blocoCor[0].classList.add('selected')
 }
+function pintandoPixel () {
+    let selectedPixel = document.querySelector('.selected')
+    let pixelWhite = document.querySelectorAll('.pixel')
+    for(let index = 0; index < pixelWhite.length; index ++){
+        pixelWhite[index].addEventListener('click', () => {
+            
+            selectedPixel.classList.remove('selected')
+            pixelWhite[index].className = 'pixel selected'
+            pixelWhite[index].style.backgroundColor = selectedPaint
 
-let listaDaPaleta = document.querySelector('.color');
-for(let index = 0; index < listaDaPaleta.length; index++){
-    listaDaPaleta[index].addEventListener('click', select())
-}
-function select (){
-    const listaDaPaleta = document.querySelector('.color');
-    let selecao = document.querySelector('selected')
-    console.log('Funfou')
-    for(let index = 0; index < listaDaPaleta.length; index++){
-        console.log('Funfou')
-        listaDaPaleta[index].classList.add('selected')
-        selecao = listaDaPaleta[index].style.backgroundColor;  
-        for(let i = 0; i < listaDaPaleta.length; i++) {
-            console.log('Funfou')
-            if(listaDaPaleta[index] !== listaDaPaleta[i] && listaDaPaleta[i].classList.contains('selected')){
-                listaDaPaleta[i].classList.remove('select');
-            }
-        }
+        })
     }
-    console.log('Funfou')
+    
 }
+function select () {
+    let listaDaPaleta = document.querySelectorAll('.color');
+    for(let index = 0; index < listaDaPaleta.length; index++) {
+        listaDaPaleta[index].addEventListener('click', () => {
+            const colorSelect = document.querySelector('.selected');
+            colorSelect.classList.remove('selected');
+            listaDaPaleta[index].className = 'color selected';
+            selectedPaint = listaDaPaleta[index].style.backgroundColor
+        }) //Elias me ajudou
+    }
+}
+select()
+pintandoPixel()
+
